883ab9b76c564cf1f274bb1f4185294c19497f77
tbg.barros <tbg.barros@hotmail.com>
2023-09-14 20:31:26 -0300

    aalteracao de pasta

--- one_card.py
+++ one_card.py
@@ -3,15 +3,15 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from io import BytesIO  # usei para baixar a imgem pois por url a imagem nao abre
 
-URL = 'https://www.deckofcardsapi.com/api/deck/1jm3vbs518ut/draw/?count=6'
+URL = "https://www.deckofcardsapi.com/api/deck/1jm3vbs518ut/draw/?count=6"
 
 
 def pegar_carta():
     response = requests.get(URL)
     dados_recebidos = response.json()
 
-    link_imagem1 = dados_recebidos['cards'][0]['image']
-    link_imagem2 = dados_recebidos['cards'][1]['image']
+    link_imagem1 = dados_recebidos["cards"][0]["image"]
+    link_imagem2 = dados_recebidos["cards"][1]["image"]
 
     imagem_carta1 = Image.open(BytesIO(requests.get(link_imagem1).content))
     foto1 = ImageTk.PhotoImage(imagem_carta1)
@@ -19,7 +19,7 @@ def pegar_carta():
     imagem_carta2 = Image.open(BytesIO(requests.get(link_imagem2).content))
     foto2 = ImageTk.PhotoImage(imagem_carta2)
 
-    if label_imagem.config('image')[-1] == str(foto1):
+    if label_imagem.config("image")[-1] == str(foto1):
         label_imagem.config(image=foto2)
     else:
         label_imagem.config(image=foto1)
