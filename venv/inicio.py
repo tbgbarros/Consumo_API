3fa571286d3e42f1b33a5f5e76299c53952a140d
tbg.barros <tbg.barros@hotmail.com>
2023-09-13 21:57:33 -0300

    corrigido pois nao dava pra pegar a imagem url

--- venv/inicio.py
+++ venv/inicio.py
@@ -1,79 +1,38 @@
-
-
-################################################
-import pprint
 import requests
 import tkinter as tk
 from PIL import Image, ImageTk
+from io import BytesIO  #usei para baixar a imgem pois por url a imagem nao abre
 
 URL = 'https://www.deckofcardsapi.com/api/deck/1jm3vbs518ut/draw/?count=2'
 
-req = requests.get(URL)
-dados_recebidos = req.json()
-pprint.pprint(dados_recebidos)
-pprint.pprint(dados_recebidos['cards'][0]['image'])
-print(dados_recebidos['cards'][1]['image'])
-# Função para atualizar a imagem da carta
 def pegar_carta():
+    response = requests.get(URL)
+    dados_recebidos = response.json()
+    
     link_imagem1 = dados_recebidos['cards'][0]['image']
-    imagem_carta1 = Image.open(link_imagem1)
-    pprint.pprint(imagem_carta1)
+    link_imagem2 = dados_recebidos['cards'][1]['image']
+
+    imagem_carta1 = Image.open(BytesIO(requests.get(link_imagem1).content))
     foto1 = ImageTk.PhotoImage(imagem_carta1)
     
-    link_imagem2 = dados_recebidos['cards'][1]['image']
-    imagem_carta2 = Image.open(link_imagem2)
+    imagem_carta2 = Image.open(BytesIO(requests.get(link_imagem2).content))
     foto2 = ImageTk.PhotoImage(imagem_carta2)
 
-    # Alterne entre as duas imagens ao pressionar o botão
     if label_imagem.config('image')[-1] == str(foto1):
         label_imagem.config(image=foto2)
     else:
         label_imagem.config(image=foto1)
 
-    label_imagem.image = foto1  # Mantém uma referência à imagem para evitar que seja coletada pelo garbage collector
+    label_imagem.image = foto1 
 
-# Cria uma janela
 janela = tk.Tk()
 janela.title("Carta de Baralho")
 
-# Botão para pegar a carta
+
 botao = tk.Button(janela, text="Pegar Carta", command=pegar_carta)
 botao.pack()
 
-# Label para exibir a imagem da carta
 label_imagem = tk.Label(janela)
 label_imagem.pack()
 
-# Inicia a interface gráfica
 janela.mainloop()
-
-
-
-
-
-
-
-
-
-
-# pprint.pprint(dados_recebidos.keys())
-# pprint.pprint(dados_recebidos['deck_id'])
-# pprint.pprint(dados_recebidos['remaining'])
-# pprint.pprint(dados_recebidos['shuffled'])
-# pprint.pprint(dados_recebidos['success'])
-# #pprint.pprint(dados_recebidos['piles'])
-
-#print(req.status_code)
-#print(dados_recebidos)
-#requests.models.Response
-
-#if req.status_code == 200:
-#    print('Requisição bem sucedida')
-#print('-----------------------')
-#for pokemon in dados_recebidos['pokemon_species']:
-#     print(pokemon['name'])
-# print('-----------------------')
-# for pokemon in dados_recebidos['types']:
-#     print(pokemon['name'])     
-# print('-----------------------')
-
