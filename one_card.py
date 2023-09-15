74006b60b04dd45ba4fb8aa1737ffa82eead8567
tbg.barros <tbg.barros@hotmail.com>
2023-09-14 21:14:53 -0300

    codigos de erro adicionados

--- one_card.py
+++ one_card.py
@@ -3,28 +3,34 @@ import tkinter as tk
 from PIL import Image, ImageTk
 from io import BytesIO  # usei para baixar a imgem pois por url a imagem nao abre
 
+
 URL = "https://www.deckofcardsapi.com/api/deck/1jm3vbs518ut/draw/?count=6"
+
+response = requests.get(URL)
+
+if response.status_code == 404:
+    print("Não encontrado")
 
 
 def pegar_carta():
-    response = requests.get(URL)
-    dados_recebidos = response.json()
+    if response.status_code == 200:
+        dados_recebidos = response.json()
 
-    link_imagem1 = dados_recebidos["cards"][0]["image"]
-    link_imagem2 = dados_recebidos["cards"][1]["image"]
+        link_imagem1 = dados_recebidos["cards"][0]["image"]
+        link_imagem2 = dados_recebidos["cards"][1]["image"]
 
-    imagem_carta1 = Image.open(BytesIO(requests.get(link_imagem1).content))
-    foto1 = ImageTk.PhotoImage(imagem_carta1)
+        imagem_carta1 = Image.open(BytesIO(requests.get(link_imagem1).content))
+        foto1 = ImageTk.PhotoImage(imagem_carta1)
 
-    imagem_carta2 = Image.open(BytesIO(requests.get(link_imagem2).content))
-    foto2 = ImageTk.PhotoImage(imagem_carta2)
+        imagem_carta2 = Image.open(BytesIO(requests.get(link_imagem2).content))
+        foto2 = ImageTk.PhotoImage(imagem_carta2)
 
-    if label_imagem.config("image")[-1] == str(foto1):
-        label_imagem.config(image=foto2)
-    else:
-        label_imagem.config(image=foto1)
+        if label_imagem.config("image")[-1] == str(foto1):
+            label_imagem.config(image=foto2)
+        else:
+            label_imagem.config(image=foto1)
 
-    label_imagem.image = foto1
+        label_imagem.image = foto1
 
 
 janela = tk.Tk()
